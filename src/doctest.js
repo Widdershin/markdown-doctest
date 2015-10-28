114da4af94f4352d3acc279951fdaa4143714059
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-29 09:05:19 +1300

    Extract markdown parsing to separate module

--- src/doctest.js
+++ src/doctest.js
@@ -2,6 +2,7 @@
 
 let fs = require('fs');
 let process = require('process');
+let parseCodeSnippets = require('./parse-code-snippets-from-markdown');
 
 function runTests (files) {
   return flattenArray(files
@@ -14,84 +15,6 @@ function runTests (files) {
 
 function read (fileName) {
   return {contents: fs.readFileSync(fileName, 'utf8'), fileName};
-}
-
-let isStartOfSnippet = line => line.trim().match(/```\W*js/);
-let isEndOfSnippet = line => line.trim() === '```';
-let isSkip = line => line.trim() === '<!-- skip-test -->';
-
-function startNewSnippet (snippets, fileName, lineNumber) {
-  let skip = snippets.skip;
-  snippets.skip = false;
-
-  return snippets.concat([
-    {code: '', fileName, lineNumber, complete: false, skip}
-  ]);
-}
-
-function addLineToLastSnippet (line) {
-  return (snippets) => {
-    let lastSnippet = snippets[snippets.length - 1];
-
-    if (lastSnippet && !lastSnippet.complete) {
-      lastSnippet.code += line + '\n';
-    }
-
-    return snippets;
-  };
-}
-
-function endSnippet (snippets, fileName, lineNumber) {
-  let lastSnippet = snippets[snippets.length - 1];
-
-  if (lastSnippet) {
-    lastSnippet.complete = true;
-  }
-
-  return snippets;
-}
-
-function skip (snippets) {
-  snippets.skip = true;
-
-  return snippets;
-}
-
-function parseLine (line) {
-  if (isStartOfSnippet(line)) {
-    return startNewSnippet;
-  }
-
-  if (isEndOfSnippet(line)) {
-    return endSnippet;
-  }
-
-  if (isSkip(line)) {
-    return skip;
-  }
-
-  return addLineToLastSnippet(line);
-}
-
-function parseCodeSnippets (args) {
-  let contents = args.contents;
-  let fileName = args.fileName;
-
-  let codeSnippets = contents
-    .split('\n')
-    .map(parseLine)
-    .reduce((snippets, lineAction, index) => lineAction(snippets, fileName, index + 1), []);
-
-  let lastSnippet = codeSnippets[codeSnippets.length - 1];
-
-  if (lastSnippet && !lastSnippet.complete) {
-    throw new Error('Snippet parsing was incomplete');
-  }
-
-  return {
-    fileName,
-    codeSnippets: codeSnippets.map(cleanUpSnippet).filter(ignorePseudocode)
-  };
 }
 
 function ignorePseudocode (snippet) {
@@ -109,7 +32,7 @@ function testFile (args) {
 
 function test (filename) {
   return (codeSnippet) => {
-    if (codeSnippet.skip) { 
+    if (codeSnippet.skip) {
       return {status: 'skip', codeSnippet, stack: ''};
     }
 
---
+++ src/parse-code-snippets-from-markdown.js
@@ -0,0 +1,89 @@
+'use strict';
+
+let isStartOfSnippet = line => line.trim().match(/```\W*js/);
+let isEndOfSnippet = line => line.trim() === '```';
+let isSkip = line => line.trim() === '<!-- skip-test -->';
+
+function cleanUpSnippet (codeSnippet) {
+  let code = codeSnippet.code
+    .replace('```js', '')
+    .replace('```', '');
+
+  return Object.assign({}, codeSnippet, {code});
+}
+
+function startNewSnippet (snippets, fileName, lineNumber) {
+  let skip = snippets.skip;
+  snippets.skip = false;
+
+  return snippets.concat([
+    {code: '', fileName, lineNumber, complete: false, skip}
+  ]);
+}
+
+function addLineToLastSnippet (line) {
+  return (snippets) => {
+    let lastSnippet = snippets[snippets.length - 1];
+
+    if (lastSnippet && !lastSnippet.complete) {
+      lastSnippet.code += line + '\n';
+    }
+
+    return snippets;
+  };
+}
+
+function endSnippet (snippets, fileName, lineNumber) {
+  let lastSnippet = snippets[snippets.length - 1];
+
+  if (lastSnippet) {
+    lastSnippet.complete = true;
+  }
+
+  return snippets;
+}
+
+function skip (snippets) {
+  snippets.skip = true;
+
+  return snippets;
+}
+
+function parseLine (line) {
+  if (isStartOfSnippet(line)) {
+    return startNewSnippet;
+  }
+
+  if (isEndOfSnippet(line)) {
+    return endSnippet;
+  }
+
+  if (isSkip(line)) {
+    return skip;
+  }
+
+  return addLineToLastSnippet(line);
+}
+
+function parseCodeSnippets (args) {
+  let contents = args.contents;
+  let fileName = args.fileName;
+
+  let codeSnippets = contents
+    .split('\n')
+    .map(parseLine)
+    .reduce((snippets, lineAction, index) => lineAction(snippets, fileName, index + 1), []);
+
+  let lastSnippet = codeSnippets[codeSnippets.length - 1];
+
+  if (lastSnippet && !lastSnippet.complete) {
+    throw new Error('Snippet parsing was incomplete');
+  }
+
+  return {
+    fileName,
+    codeSnippets: codeSnippets.map(cleanUpSnippet)
+  };
+}
+
+module.exports = parseCodeSnippets;
